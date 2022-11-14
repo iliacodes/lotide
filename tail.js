c43a4850a3ec0ebae4d76cebf06207e8e73134dc
iliacodes <6ac177ed@protonmail.com>
2022-11-14 13:40:36 -0800

    creating a function that removes the first element of an array

---
+++ tail.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return (`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  }
+  else {
+    return (`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+};
+
+
+const tail = function(arr){
+  return arr.slice(1);
+};
