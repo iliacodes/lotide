a51e4f0df05e4e955642197fe8afbb3b845777eb
iliacodes <6ac177ed@protonmail.com>
2022-11-15 13:46:37 -0800

    created a function to compare two arrays utilizing assertEqual function

---
+++ assertArraysEqual.js
@@ -0,0 +1,22 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return (`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  }
+  else {
+    return (`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const assertEqualArrays = function(arr1, arr2) {return (assertEqual(arr1,arr2))}
