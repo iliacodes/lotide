af319a95ec2d6d71b0c30e3e7691afd118223bf4
iliacodes <6ac177ed@protonmail.com>
2022-11-15 13:39:10 -0800

    created a function to compare two arrays if they are equal in length and values

---
+++ eqArrays.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return (`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
+  }
+  else {
+    return (`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
+  }
+};
+
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
