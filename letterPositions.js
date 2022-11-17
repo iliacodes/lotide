177743896e3282a4506a68de50f1a10db86c252e
iliacodes <6ac177ed@protonmail.com>
2022-11-16 16:53:58 -0800

    created a function to list all the indices of the letter in a string

---
+++ letterPositions.js
@@ -0,0 +1,14 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  let stringWithoutWhitespace = sentence.replace(/\s/g, "");
+  let letters = stringWithoutWhitespace.split("")
+  letters.forEach((element, index) => {
+    if (results[element]) {
+      results[element].push(index);
+    }
+    else {
+      results[element] = [index];
+    }
+  })
+  return results;
+};
