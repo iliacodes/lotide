fba21eee6a70e384be7308df29f26991b5a4aed0
iliacodes <6ac177ed@protonmail.com>
2022-11-21 14:59:25 -0800

    made export itself

--- head.js
+++ head.js
@@ -1,14 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return (`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-  }
-  else {
-    return (`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
-  }
-};
-
+const assertEqual = require('./assertEqual');
 
 const head = function(arr) {
     return arr[0];
 };
 
+module.exports = head;
