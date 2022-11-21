dd8fe13d4a1193a3ccbcf33212bda302347fb712
iliacodes <6ac177ed@protonmail.com>
2022-11-21 14:58:59 -0800

    made export

--- flatten.js
+++ flatten.js
@@ -1,17 +1,8 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-const assertEqualArrays = function(arr1, arr2) {return (assertEqual(arr1,arr2))}
+const eqArrays = require('./eqArrays');
+const assertArraysEqual = require('./assertArraysEqual')
 
 const flatten = function (array) {
   return array.flat();
 };
+
+module.exports = flatten;
