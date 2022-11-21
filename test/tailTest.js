1932c33a1aead61daf37ee36e1aa88948ec0badb
iliacodes <6ac177ed@protonmail.com>
2022-11-21 15:05:45 -0800

    created test file

---
+++ test/tailTest.js
@@ -0,0 +1,12 @@
+const tail = require('../tail');
+const assert = require('chai').assert;
+
+describe('#tail', () => {
+  it("returns [2, 3] for [1, 2, 3]", () => {
+    assert.deepEqual(tail([1, 2, 3]), [2, 3]);
+  });
+
+  it("return [], for [5]", () => {
+    assert.deepEqual(tail([5]), []);
+  });
+});
