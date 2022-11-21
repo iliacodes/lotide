f27badbd9a8a42ee5f0f34007324d72e3569bdb0
iliacodes <6ac177ed@protonmail.com>
2022-11-21 15:00:59 -0800

    made file export itself

--- map.js
+++ map.js
@@ -1,6 +1,3 @@
-
-const words = ["ground", "control", "to", "major", "tom"];
-
 const map = function (array, callback) {
   let result = []
   for (let element of array) {
@@ -9,5 +6,4 @@ const map = function (array, callback) {
   return result;
 }
 
-const results1 = map(words, word => word[0]);
-console.log(results1);
+module.exports = map;
