5ed6b5daa7f14a4902de4a9cbdfa4bd0333c3926
iliacodes <6ac177ed@protonmail.com>
2022-11-21 14:55:34 -0800

    made code import assertEqual adn export itself

--- countOnly.js
+++ countOnly.js
@@ -1,24 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return (`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-  }
-  else {
-    return (`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
-  }
-};
-
-// const countOnly = function (allItems, itemsToCount) {
-//   const results = {};
-//   for (item of allItems) {
-//     if (!results[item]) {
-//       results[item] = 1;
-//     }
-//     else {
-//       results[item] += 1
-//     } 
-//   }
-//   console.log(results);
-// }
+const assertEqual = require('./assertEqual')
 
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
@@ -28,24 +8,6 @@ const countOnly = function(allItems, itemsToCount) {
     }
   }
   return results;
-}
+};
 
-
-const firstNames = [
-  "Karl",
-  "Salima",
-  "Agouhanna",
-  "Fang",
-  "Kavith",
-  "Jason",
-  "Salima",
-  "Fang",
-  "Joe"
-];
-
-const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-
-// assertEqual(result1["Jason"], 1);
-console.log(assertEqual(result1["Karima"], undefined));
-console.log(assertEqual(result1["Fang"], 2));
-console.log(assertEqual(result1["Agouhanna"], undefined));
+module.exports = countOnly;
