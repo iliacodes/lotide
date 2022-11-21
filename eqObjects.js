2e6a48b7200bac20a0e5f8340d412dab23cae749
iliacodes <6ac177ed@protonmail.com>
2022-11-21 14:56:49 -0800

    made code import assertequal and eqarrays along with exporting itself

--- eqObjects.js
+++ eqObjects.js
@@ -1,29 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return (`✅✅✅ Assertion Passed: ${actual} === ${expected}`)
-  }
-  else {
-    return (`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`)
-  }
-};
-
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
+const assertEqual = require('./assertEqual')
+const eqArrays = require('./eqArrays')
 
 const eqObjects = function(object1, object2) {
-  // see if both objects have same amount of keys (Object.keys method)
-  // check if value for each key in one object is the same as in other object
-  // loop through object
   let keys1 = Object.keys(object1);
   let keys2 = Object.keys(object2);
   if (keys1.length !== keys2.length) {
@@ -43,10 +21,4 @@ const eqObjects = function(object1, object2) {
   return true;
 };
 
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-console.log(eqObjects(cd, dc)); // => true
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-console.log(eqObjects(cd, cd2))
+module.exports = eqObjects;
