6a3b667d122afc00a49fdf64237b7de58c1b938c
iliacodes <6ac177ed@protonmail.com>
2022-11-21 14:53:16 -0800

    refactored code to import eqObjects and export itself

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -1,22 +1,4 @@
-const eqObjects = function(object1, object2) {
-  let keys1 = Object.keys(object1);
-  let keys2 = Object.keys(object2);
-  if (keys1.length !== keys2.length) {
-    return false;
-  }
-  for (let key of keys1) {
-    if (Array.isArray(object1[key]) && Array.isArray(object2[key])){
-      let result = eqArrays(object1[key], object2[key])
-      if (!result) {
-        return false;
-      }
-    }
-      else if (object1[key] !== object2[key]) {
-        return false;
-    }
-  }
-  return true;
-};
+const eqObjects = require('./eqObjects');
 
 const assertObjectEqual = function(actual, expected) {
   const inspect = require('util').inspect;
@@ -28,4 +10,4 @@ const assertObjectEqual = function(actual, expected) {
   }
 };
 
-console.log(assertObjectEqual({1:2, 3:4}, {1:2, 3:4}))
+module.exports = assertObjectEqual;
