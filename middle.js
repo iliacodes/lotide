e4bddf763e23e8e28c319934fda4822604a96710
iliacodes <6ac177ed@protonmail.com>
2022-11-21 15:01:22 -0800

    made file export itself

--- middle.js
+++ middle.js
@@ -1,23 +1,5 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-}
-
-
-const assertArrayEqual = function(arr1, arr2) {return (assertEqual(arr1,arr2))}
-
-
-//function that finds middle element of an array
-// if the array is even - return both middle numbers
-// if array is odd - return one number
-// if an array has one or two numbers - return an empty array
+const assertArraysEqual = require('./assertArraysEqual');
+const eqArrays = require('./eqArrays')
 
 const middle = function(array) {
   let arrayLength = array.length;
@@ -36,3 +18,5 @@ const middle = function(array) {
   }
   return result;
 };
+
+module.exports = middle;
